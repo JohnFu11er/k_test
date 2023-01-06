a2598b5b5cd2539714179a0ea07ff433eb71e501
JohnFu11er <62799830+JohnFu11er@users.noreply.github.com>
2023-01-06 18:45:36 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
 #############################
 # Basic node configuration
 
+touch build_output.txt
+
+echo "starting build" >> build_output.txt
+
 swapoff -a
 
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
@@ -22,45 +28,8 @@ sudo sysctl --system
 sudo apt-get update 
 sudo apt-get install -y containerd
 
-sudo mkdir -p /etc/containerd
-sudo containerd config default | sudo tee /etc/containerd/config.toml
-
-sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
-
-sudo systemctl restart containerd
-
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sudo apt-get update
-
-VERSION=1.24.3-00
-sudo apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION
-sudo apt-mark hold kubelet kubeadm kubectl containerd
-
-sudo systemctl enable kubelet.service
-sudo systemctl enable containerd.service
-swapoff -a
-
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-overlay
-br_netfilter
-EOF
-
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
-EOF
-
-sudo sysctl --system
-
-sudo apt-get update 
-sudo apt-get install -y containerd
+echo ""
+echo "containerd installed" >> build_output.txt
 
 sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
@@ -82,6 +51,8 @@ sudo apt-mark hold kubelet kubeadm kubectl containerd
 sudo systemctl enable kubelet.service
 sudo systemctl enable containerd.service
 
+echo ""
+echo "base node build complete" >> build_output.txt
 
 #############################
 # Control-node configuration
@@ -89,3 +60,6 @@ sudo systemctl enable containerd.service
 wget https://docs.projectcalico.org/manifests/calico.yaml
 
 sudo kubeadm init --kubernetes-version v1.24.3
+
+echo ""
+echo "kuberenetes build compete" >> build_output.txt
